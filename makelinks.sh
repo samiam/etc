577cb6d98bc8ff266c85b6935a982cf6c86da9d9
Sam <samnap+github@gmail.com>
2014-11-14 23:26:05 -0500

    Rewrite setting up links

--- makelinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-DOTDIR="$(pwd)"
-
-ln -s $DOTDIR/bash_profile     ~/.bash_profile 
-ln -s $DOTDIR/bin              ~/bin
-ln -s $DOTDIR/emacs.d          ~/.emacs.d
-ln -s $DOTDIR/gitconfig        ~/.gitconfig
-ln -s $DOTDIR/irbrc            ~/.irbrc
