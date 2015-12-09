96520ce17e354e273e0966cfbbcf3e41999f9da1
Sam <samnap+github@gmail.com>
2015-12-09 16:37:12 -0500

    Small tweaks

--- bashrc
+++ bashrc
@@ -143,3 +143,6 @@ fi
 if which rbenv > /dev/null; then
   eval "$(rbenv init -)";
 fi
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
--- emacs.d/lisp/dotemacs.el
+++ emacs.d/lisp/dotemacs.el
@@ -16,3 +16,7 @@
 
 (add-to-list 'load-path "~/.emacs.d/xquery-mode")
 (load "xquery-setup.el")
+
+(add-to-list 'load-path "~/.emacs.d/rust-mode")
+(autoload 'rust-mode "rust-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
